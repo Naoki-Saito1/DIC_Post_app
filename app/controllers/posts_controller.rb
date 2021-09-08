001f5ab497e81880ee3906f20be074d9f3cec34e
saito naoki <reisa.naoki@icloud.com>
2021-09-08 20:37:19 +0900

    バリデーションや細かい機能の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,21 @@
 class PostsController < ApplicationController
-　before_action :set_post, only: [:show, :edit, :update, :destroy]
+before_action :set_post, only: [:show, :edit, :update, :destroy]
 def index
-　@posts = Post.all
+@posts = Post.all
 end
 def new
-　@post=Post.new
+@post=Post.new
 end  
 def create
- 　@post = Post.new(post_params)
+ @post = Post.new(post_params)
 if params[:back]
-  render :new
+render :new
 else  
 if @post.save
 
-　redirect_to posts_path, notice: "記事を投稿しました！"
+redirect_to posts_path, notice: "記事を投稿しました！"
 else
-　render :new
+render :new
 end
 end 
 end
@@ -25,25 +25,25 @@ def edit
 end
 def update
 if @post.update(post_params)
-   redirect_to posts_path, notice: "記事をを編集しました！"
+redirect_to posts_path, notice: "記事をを編集しました！"
 else
-　 render :edit
+render :edit
 end
 end
 def confirm
-　 @post = Post.new(post_params)
-　 render :new if @post.invalid?
+@post = Post.new(post_params)
+render :new if @post.invalid?
 end
 def destroy
   
-　 @post.destroy
-　 redirect_to posts_path, notice:"記事をを削除しました！"
+@post.destroy
+redirect_to posts_path, notice:"記事をを削除しました！"
 end
 private
 def post_params
-  params.require(:post).permit(:content)
+params.require(:post).permit(:content)
 end
 def set_post
-  @post = Post.find(params[:id])
+@post = Post.find(params[:id])
 end 
 end
