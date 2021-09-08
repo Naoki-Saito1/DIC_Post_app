fbe44c3974e308333d6b64e7d89739b92801a3b2
saito naoki <reisa.naoki@icloud.com>
2021-09-08 14:26:39 +0900

    CSSの編集を行った

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-  def index
-    @posts = Post.all
-  end
+before_action :set_post, only: [:show, :edit, :update, :destroy]
+def index
+@posts = Post.all
+end
 def new
 @post=Post.new
 end  
